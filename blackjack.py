970ce0fc0b077c88f6e575a6d10948e84b7774dc
DaLama007 <marwan.lamesch@gmail.com>
2025-07-02 22:43:21 +0200

    finish UI and debut of bj logic

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,21 @@
 import random
 
+class Blackjack:
+    def __init__(self):
+        self.state = "start"
+        self.points_dealer = 0
+        self.points_player = 0
+
+    def hit():
+        pass
+
+    def double():
+        pass
+
+    def stand():
+        pass
+
+    def iswon(self):
+        if self.state == "true":
+            pass
+    
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QMainWindow, QWidget, QGridLayout
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit
+from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import Qt
+
 import sys
+from blackjack import Blackjack
 
 class Main(QMainWindow):
     def __init__(self):
@@ -12,6 +14,8 @@ class Main(QMainWindow):
         
 
     def settings(self):
+        custom_font = QFont("Times", 30, QFont.Bold)
+        QApplication.setFont(custom_font)
         self.setWindowTitle("Casino")
         self.showFullScreen()
         
@@ -19,29 +23,67 @@ class Main(QMainWindow):
     def initUi(self):
         central_widget = QWidget()
         userMoney = QLabel("Money")
-        userCard1 = QLabel()
-        userCard2 = QLabel()
+        current_bet = QLabel('Curr Bet:')
+        label_bet = QLabel('Bet:')
+        input_bet = QLineEdit()
+        userCard1 = QLabel('card1')
+        userCard2 = QLabel('card2')
+        Instruction1 = QLabel("H for Hit")
+        Instruction2 = QLabel("S for Stand")
+        Instruction3 = QLabel("D for Double")
         
 
         button_hit = QPushButton()
         button_stand = QPushButton()
         button_double = QPushButton()
 
-        main = QGridLayout()
-        central_widget.setLayout(main)
-        main.addWidget(userMoney, 5, 2)
-        main.addWidget(userCard1, 5, 1)
-        #main.addWidget(userMoney, 0, 0)
         
-        self.setCentralWidget(central_widget)
+        row1 = QHBoxLayout()
+        row2 = QHBoxLayout()
+        row3 = QHBoxLayout()
+        row4 = QHBoxLayout()
+        row5 = QHBoxLayout()
+        row6 = QHBoxLayout()
+        row7 = QHBoxLayout()
+        
+        
+        
+        row1.addWidget(userMoney)
+        row6.addWidget(label_bet)
+        row6.addWidget(input_bet)
+        row5.addWidget(userCard1)
+        row5.addWidget(userCard2)
+        row5.addWidget(current_bet)
+        row7.addWidget(Instruction1)
+        row7.addWidget(Instruction2)
+        row7.addWidget(Instruction3)
+        
+        master= QVBoxLayout()
+        master.addLayout(row1)
+        master.addLayout(row2)
+        master.addLayout(row3)
+        master.addLayout(row4)
+        master.addLayout(row5)
+        master.addLayout(row6)
+        master.addLayout(row7)
 
-        
+        central_widget.setLayout(master)
+        self.setCentralWidget(central_widget)
     
+    def updateCards(self):
+        self.userCard1
+        self.userCard2
+        self.userCard3
+        self.userCard4
+        self.userCard5
+        self.userCard6
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_F11:
             self.showNormal()
         elif e.key() == Qt.Key_Escape:
             self.close()
+        elif e.key() == Qt.Key_H:
+            Blackjack.hit()
 
 
         
